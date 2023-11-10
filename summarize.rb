86bc59c847e741a312c50c2754570bd64232e34b
Mike Dallas <mcdallas@protonmail.com>
2023-11-10 01:30:40 +0000

    v0.2.0

--- summarize.rb
+++ summarize.rb
@@ -1,8 +1,8 @@
 class Summarize < Formula
   desc "Command-line tool to summarize audio/video files"
   homepage "https://github.com/mcdallas/summarize"
-  url "https://github.com/mcdallas/summarize/releases/download/v0.1.0/summarize_v0.1.0_x86_64-apple-darwin.tar.gz"
-  sha256 "f2294b833dcfd270f550a163817a95143caa16e63b136d70ad2ce11e18fb23cd"
+  url "https://github.com/mcdallas/summarize/releases/download/v0.2.0/summarize_v0.2.0_x86_64-apple-darwin.tar.gz"
+  sha256 "2ded9335d52ea4d5484c668efe33947799c8e63b2a7e462cc69a1f99f00c709b"
   license "MIT"
 
   def install
